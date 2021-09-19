f4250165dde57e45996bb68801bec5549c3aa832
taylor <taylor.phebillo@gmail.com>
2021-09-19 12:26:13 -0500

    in progress day3 part 2

--- day3.jl
+++ day3.jl
@@ -1,13 +1,29 @@
-treecount = 0
-col = 1
-for row in readlines("day3.txt")
-  if row[col] == '#'
-    global treecount += 1
+function checkTrees(rowΔ, colΔ)
+  treecount = 0
+  col = 1
+  rownum = 0
+  for row in readlines("day3.txt")
+    rownum += 1
+    if (rownum % rowΔ) == 0
+
+      if row[col] == '#'
+        treecount += 1
+      end
+    end
+#    println(row, " ", col, " ", row[col])
+    col = (col + colΔ) % (lastindex(row)+1)
+    if col < colΔ
+      col += 1
+    end
   end
-  println(row, " ", col, " ", row[col])
-  global col = (col + 3) % (lastindex(row)+1)
-  if col < 3
-    global col += 1
-  end
+  return treecount
 end
-println(treecount)
+
+
+println(
+	checkTrees(1, 1), " ",
+	checkTrees(1, 3), " ",
+	checkTrees(1, 5), " ",
+	checkTrees(1, 7), " ",
+	checkTrees(2, 1))
+
