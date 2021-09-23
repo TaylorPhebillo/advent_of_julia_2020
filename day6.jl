e5a4516d880b4ce893d5631d0d737a35957fc27f
taylor <taylor.phebillo@gmail.com>
2021-09-23 17:45:18 -0500

    day6 done Two gold stars!

--- day6.jl
+++ day6.jl
@@ -1,14 +1,22 @@
 function count(collected)
+  println(collected, " ", length(collected))
+
   return length(collected)
 end
 function day6()
   sumcount = 0
   collected = Set([])
+  init = true
   for line in readlines("day6.txt")
     println(collected)
-    if length(line) == 0 || length(collected) == 0
-      sumcount += count(collected)
+    if init
       collected = Set(line)
+      init = false
+    end
+    if length(line) == 0
+      init = true
+      sumcount += count(collected)
+      #collected = Set(line)
     end
     intersect!(collected, line)
   end
