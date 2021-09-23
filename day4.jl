8a83acb0cb346ee4ab237eea4ee0b9bbdd3ee1df
taylor <taylor.phebillo@gmail.com>
2021-09-23 17:07:37 -0500

    First pass at day4

---
+++ day4.jl
@@ -0,0 +1,30 @@
+function day4()
+valid_count = 0
+required_set = Set([
+  "byr",
+  "iyr",
+  "eyr",
+  "hgt",
+  "hcl",
+  "ecl",
+  "pid"])
+#  "cid",
+contains = Set([])
+for line in readlines("day4.txt")
+  if length(line) == 0 
+    if length(setdiff(required_set, contains)) == 0
+      valid_count += 1
+    end
+    println(required_set, " ", contains)
+    contains = Set([])
+    continue
+  end
+  for field in split(line, " ")
+    println(field)
+    union!(contains, [split(field, ":")[1]])
+  end
+  println(line)
+end
+print(valid_count)
+end
+day4()
